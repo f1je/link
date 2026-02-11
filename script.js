482f103d3c876b628d64355c74f10a8a287b94dd
f1je <137819891+f1je@users.noreply.github.com>
2026-02-11 04:38:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 
+document.getElementById("tryoutForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    document.getElementById("successMsg").textContent = 
+        "Application submitted! We will contact you on Roblox.";
+});
