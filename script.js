793da9c06e1ef0946c21b1fcae36332e00f41b74
f1je <137819891+f1je@users.noreply.github.com>
2026-02-11 04:50:32 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("form").addEventListener("submit", function(e){
-    e.preventDefault();
-    document.getElementById("success").textContent =
-        "Application Sent! LINK Staff will contact you.";
-});
